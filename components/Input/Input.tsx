eec4502375aeaeb2a73436db8360aa096d177566
an donghyun <ax34554@gmail.com>
2025-04-12 18:06:32 +0900

    feat: Input 공통 컴포넌트에 Label 생성

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -3,18 +3,14 @@ import { Controller, useFormContext } from 'react-hook-form';
 import type { FieldPath, FieldValues } from 'react-hook-form';
 
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
-export interface InputProps<T extends FieldValues>
-  extends React.ComponentProps<'input'> {
+export interface InputProps<T extends FieldValues> extends React.ComponentProps<'input'> {
   name: FieldPath<T>;
+  label: string;
 }
 
-const CustomInput = <T extends FieldValues>({
-  name,
-  className,
-  type,
-  ...props
-}: InputProps<T>) => {
+const CustomInput = <T extends FieldValues>({ name, className, type, label, ...props }: InputProps<T>) => {
   const method = useFormContext<T>();
 
   const { control } = method;
@@ -24,13 +20,12 @@ const CustomInput = <T extends FieldValues>({
       control={control}
       name={name}
       render={({ field: { onChange, ...field } }) => (
-        <Input
-          className={className}
-          type={type}
-          {...props}
-          {...field}
-          onChange={onChange}
-        />
+        <div className="flex flex-col gap-2.5">
+          <Label htmlFor={name} className="text-sm text-gray-600">
+            {label}
+          </Label>
+          <Input id={name} className={className} type={type} {...props} {...field} onChange={onChange} />
+        </div>
       )}
     />
   );
