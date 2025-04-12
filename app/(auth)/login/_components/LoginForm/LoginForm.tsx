76db5bea88e413e58621bf738ff7eadac83389d6
an donghyun <ax34554@gmail.com>
2025-04-12 18:04:50 +0900

    feat: 로그인 페이지 1. 진행중..

--- app/(auth)/login/_components/LoginForm/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm/LoginForm.tsx
@@ -34,10 +34,12 @@ const LoginForm = () => {
 
   return (
     <FormProvider {...method}>
-      <form onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="email@email.com" />
-        <Input name="password" type="password" placeholder="비밀번호" />
-        <Button type="submit">로그인</Button>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <Input type="email" label="이메일" name="email" placeholder="이메일을 입력해주세요." />
+        <Input type="password" label="비밀번호" name="password" placeholder="비밀번호를 입력해주세요." />
+        <Button className="h-12 text-md rounded-xl" type="submit">
+          로그인
+        </Button>
       </form>
       <ErrorMessage>{getLoginErrorMessage(errors)}</ErrorMessage>
     </FormProvider>
