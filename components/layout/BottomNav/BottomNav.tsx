4e686577ea6f10ff280851548014f2165822f77e
an donghyun <ax34554@gmail.com>
2025-04-13 15:36:25 +0900

    fix: 정적파일 구조 변경

--- components/layout/BottomNav/BottomNav.tsx
+++ components/layout/BottomNav/BottomNav.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import { type StaticImageData } from 'next/image';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
@@ -15,8 +14,8 @@ interface NavItemProps {
   href: string;
   segment: string | null;
   activeSegment: string | null;
-  activeIcon: StaticImageData;
-  inactiveIcon: StaticImageData;
+  activeIcon: string;
+  inactiveIcon: string;
   label: string;
 }
 
@@ -28,7 +27,7 @@ const NavItem = ({ href, segment, activeSegment, activeIcon, inactiveIcon, label
       <Button variant="ghost" size="icon" className="hover:bg-transparent" asChild>
         <Link href={href}>
           <div className="flex flex-col items-center space-y-3">
-            <Image src={isActive ? activeIcon : inactiveIcon} alt={label} />
+            <Image src={isActive ? activeIcon : inactiveIcon} alt={label} width={16} height={16} priority />
             <span className={cn('text-xs', isActive ? 'text-primary' : 'text-gray500')}>{label}</span>
           </div>
         </Link>
@@ -44,15 +43,7 @@ const BottomNav = () => {
     <nav className="fixed bottom-0 left-0 right-0 z-10 max-w-[560px] mx-auto bg-white border-t border-gray-200 py-2 px-3 h-14">
       <ul className="flex justify-between">
         {NAV_ITEMS.map((item) => (
-          <NavItem
-            key={item.href}
-            href={item.href}
-            segment={segment}
-            activeSegment={item.activeSegment}
-            activeIcon={item.activeIcon}
-            inactiveIcon={item.inactiveIcon}
-            label={item.label}
-          />
+          <NavItem key={item.href} segment={segment} {...item} />
         ))}
       </ul>
     </nav>
--- constants/paths.ts
+++ constants/paths.ts
@@ -1,4 +1,31 @@
 export const PATH = Object.freeze({
+  /**
+   * 홈
+   */
   root: '/' as const,
+
+  /**
+   * 로그인
+   */
   login: '/login' as const,
+
+  /**
+   * 날씨비교
+   */
+  compare: '/compare' as const,
+
+  /**
+   * 친구
+   */
+  friend: '/friend' as const,
+
+  /**
+   * 채팅
+   */
+  chat: '/chat' as const,
+
+  /**
+   * 마이페이지
+   */
+  profile: '/profile' as const,
 });
