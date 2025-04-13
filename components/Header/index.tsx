cf068075d100e89854dbaea0aa38441006decd8c
H0onnn <dukei2017@naver.com>
2025-04-13 12:51:07 +0900

    design: header fixed 적용

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,11 @@
 import { Back, LocationDisplay, Notification, Setting, Title } from './components';
 
 const Root = ({ children }: { children: React.ReactNode }) => {
-  return <header className="min-h-[73px] bg-background px-4 py-6 border-b border-b-gray-200">{children}</header>;
+  return (
+    <header className="fixed top-0 left-0 right-0 z-10 min-h-[73px] max-w-[560px] mx-auto bg-background px-4 py-6 border-b border-b-gray-200">
+      {children}
+    </header>
+  );
 };
 
 const Header = () => null;
