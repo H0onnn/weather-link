e772a8062be8f9080c91c1efc399c6858f5b3aa2
H0onnn <dukei2017@naver.com>
2025-04-17 17:02:08 +0900

    feat: 회원가입 이메일 인증 관련 api 추가

--- app/(auth)/sign-up/actions.ts
+++ app/(auth)/sign-up/actions.ts
@@ -7,6 +7,11 @@ import { api } from '@/lib/axios';
 
 import { type SignupFormSchema, signupSchema } from './validator';
 
+/**
+ * 회원가입
+ * @param data 회원가입 데이터
+ * @returns 회원가입 성공 여부
+ */
 export const signup = async (data: FormData | SignupFormSchema) => {
   const formData = data instanceof FormData ? Object.fromEntries(data.entries()) : data;
 
@@ -37,5 +42,36 @@ export const signup = async (data: FormData | SignupFormSchema) => {
     }
 
     return { error: '알 수 없는 오류가 발생했습니다.' };
+  }
+};
+
+/**
+ * 회원가입 이메일 인증 번호 발송
+ * @param email 이메일
+ * @returns 이메일 인증 번호 발송 성공 여부
+ */
+export const sendCertEmail = async (email: string) => {
+  try {
+    await api.post('/auth/service/signup/sendcode', { email });
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return { error: error.response?.data?.message || '이메일 인증 번호 발송 실패' };
+    }
+  }
+};
+
+/**
+ * 회원가입 이메일 인증 번호 검증
+ * @param email 이메일
+ * @param code 인증 번호
+ * @returns 인증 번호 검증 성공 여부
+ */
+export const verifyCertEmail = async (email: string, code: string) => {
+  try {
+    await api.post('/auth/service/signup/verifycode', { email, code });
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return { error: error.response?.data?.message || '이메일 인증 번호 검증 실패' };
+    }
   }
 };
