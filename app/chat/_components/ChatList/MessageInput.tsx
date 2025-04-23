26d5ebd914d2dcc56b9449aeaae001e556502416
H0onnn <dukei2017@naver.com>
2025-04-23 11:02:32 +0900

    feat: 채팅 페이지 ui

---
+++ app/chat/_components/ChatList/MessageInput.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Send } from 'lucide-react';
+import { useState } from 'react';
+
+import { Input } from '@/components/ui/input';
+
+export const MessageInput = () => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = () => {
+    // chatSocketManager.emit('sendMessage', {
+    //   roomId: '',
+    //   content: message,
+    // });
+    setMessage('');
+  };
+
+  return (
+    <form
+      className="fixed bottom-0 left-0 right-0 z-10 px-5 py-3 pb-17 bg-white"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
+      <div className="flex items-center space-x-2 w-full">
+        <Input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="오픈톡에 참여해보세요"
+          containerClassName="bg-gray-100"
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleSubmit();
+            }
+          }}
+        />
+        <button
+          type="submit"
+          className="rounded-full bg-primary w-10 min-w-10 h-10 min-h-10 flex items-center justify-center"
+        >
+          <Send className="w-4 h-4 text-white" />
+        </button>
+      </div>
+    </form>
+  );
+};
