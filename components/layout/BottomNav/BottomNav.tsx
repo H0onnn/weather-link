2220184f301e599de51f993075f72e7c57d9ba28
H0onnn <dukei2017@naver.com>
2025-04-24 14:57:42 +0900

    chore: 미사용 Import 삭제

--- components/layout/BottomNav/BottomNav.tsx
+++ components/layout/BottomNav/BottomNav.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useSelectedLayoutSegment, useSelectedLayoutSegments } from 'next/navigation';
+import { useSelectedLayoutSegments } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 
