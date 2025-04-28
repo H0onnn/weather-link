478043deb14fe3f60c00c66d5bfb10c007e00791
H0onnn <dukei2017@naver.com>
2025-04-28 20:08:13 +0900

    chore: query devtools 제거

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import * as React from 'react';
 
 import { getQueryClient } from '@/lib/query';
@@ -9,10 +8,5 @@ import { getQueryClient } from '@/lib/query';
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = getQueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
