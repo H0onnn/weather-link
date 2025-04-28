048bfbc25721dd4b56be2701268bd11d037669c5
H0onnn <dukei2017@naver.com>
2025-04-28 22:42:52 +0900

    fix: force dynamic

--- app/friend/request/page.tsx
+++ app/friend/request/page.tsx
@@ -2,6 +2,8 @@ import { getFriendRequestList } from '@/app/friend/_service/apis';
 
 import { FriendRequestList } from './_components/FriendRequestList';
 
+export const dynamic = 'force-dynamic';
+
 export default async function FriendRequestPage() {
   const friendReqListPromise = getFriendRequestList();
 
